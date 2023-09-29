dc239e9378549607c2ce52a809bb762f6fa8ab21
KavithaVenugopal <kavithanov.1611@gmail.com>
2023-09-29 09:50:10 +0000

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo "installing jenkins using docker file"
 sudo apt update -y
 sudo apt install git
 sudo apt install docker.io -y
+sudo apt install openjdk-ll-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+jenkins --version
+docker --version
+git --version
 
